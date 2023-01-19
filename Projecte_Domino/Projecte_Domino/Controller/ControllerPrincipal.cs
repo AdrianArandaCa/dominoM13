39ed750fd03dabc7369cff5a9915519e89ac7188
Agustin <agusti_cintassoriano@iescarlesvallbona.cat>
2023-01-19 20:20:51 +0100

    Insercion de proyecto DOMINO

---
+++ Projecte_Domino/Projecte_Domino/Controller/ControllerPrincipal.cs
@@ -0,0 +1,25 @@
+﻿using Projecte_Domino.Vista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projecte_Domino.Controller
+{
+    internal class ControllerPrincipal
+    {
+        Form1 f;
+
+        public ControllerPrincipal()
+        {
+            f = new Form1();
+
+            // initListeners();
+
+            Application.Run(f);
+        }
+
+    }
+}
